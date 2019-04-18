fe0d86b8aa1365030a633605fbdb5f782a0b958d
Learzhu <694125196@qq.com>
2019-04-18 23:28:08 +0800

    1.Flowable 的初步理解练习 2.界面修改 ConstraintLayout布局的学习

--- app/src/main/java/com/learzhu/rxandroiddemo/rxandroid_test/RxAndroidActivity7.java
+++ app/src/main/java/com/learzhu/rxandroiddemo/rxandroid_test/RxAndroidActivity7.java
@@ -9,6 +9,7 @@ import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.util.Log;
 import android.view.View;
+import android.widget.TextView;
 
 import com.learzhu.rxandroiddemo.R;
 
@@ -22,8 +23,19 @@ import io.reactivex.FlowableOnSubscribe;
 import io.reactivex.android.schedulers.AndroidSchedulers;
 import io.reactivex.schedulers.Schedulers;
 
+/**
+ * https://github.com/ssseasonnn/RxJava2Demo/blob/master/app/src/main/java/zlc/season/rxjava2demo/demo/ChapterSeven.java
+ * 我们把request当做是一种能力, 当成下游处理事件的能力, 下游能处理几个就告诉上游我要几个,
+ * 这样只要上游根据下游的处理能力来决定发送多少事件, 就不会造成一窝蜂的发出一堆事件来, 从而导致OOM.
+ * 这也就完美的解决之前我们所学到的两种方式的缺陷, 过滤事件会导致事件丢失, 减速又可能导致性能损失.
+ * 而这种方式既解决了事件丢失的问题, 又解决了速度的问题, 完美 !
+ * Flowable在设计的时候采用了一种新的思路也就是响应式拉取的方式来更好的解决上下游流速不均衡的问题,
+ */
 public class RxAndroidActivity7 extends AppCompatActivity {
     private static final String TAG = "RxAndroidActivity7";
+    private static Subscription mSubscription;
+
+    private TextView mStartTv, mRequestTv;
 
     public static void actionStart(Context context) {
         Intent intent = new Intent(context, RxAndroidActivity7.class);
@@ -42,9 +54,25 @@ public class RxAndroidActivity7 extends AppCompatActivity {
             public void onClick(View view) {
                 Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
                         .setAction("Action", null).show();
+                testFlowable1();
             }
         });
-        testFlowable1();
+        mStartTv = findViewById(R.id.tv1);
+        mRequestTv = findViewById(R.id.tv2);
+        mStartTv.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+//                testFlowable2();
+                testFlowable3();
+            }
+        });
+        mRequestTv.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                request(10);
+            }
+        });
+//        testFlowable1();
     }
 
     /**
@@ -130,6 +158,10 @@ public class RxAndroidActivity7 extends AppCompatActivity {
         });
     }
 
+    /**
+     * 下游虽然没有调用request, 但是上游在水缸中保存着这些事件, 只有当下游调用request时, 才从水缸里取出事件发给下游.
+     * 两个线程
+     */
     private void testFlowable2() {
         Flowable.create(new FlowableOnSubscribe<Integer>() {
             @Override
@@ -150,6 +182,7 @@ public class RxAndroidActivity7 extends AppCompatActivity {
                 Log.d(TAG, "onSubscribe");
 //                s.request(Long.MAX_VALUE);  //注意这句代码
                 //MissingBackpressureException 必须有
+                mSubscription = s;
             }
 
             @Override
@@ -168,5 +201,51 @@ public class RxAndroidActivity7 extends AppCompatActivity {
             }
         });
     }
+
+    /**
+     * 下游虽然没有调用request, 但是上游在水缸中保存着这些事件, 只有当下游调用request时, 才从水缸里取出事件发给下游.
+     * 两个线程
+     */
+    private void testFlowable3() {
+        Flowable.create(new FlowableOnSubscribe<Integer>() {
+            @Override
+            public void subscribe(FlowableEmitter<Integer> emitter) throws Exception {
+                for (int i = 0; i < 128; i++) {//水缸的大小为128 Flowable的源码中就有这个buffersize的大小定义
+//                for (int i = 0; i < 129; i++) {//MissingBackpressureException: create: could not emit value due to lack of requests
+                    Log.d(TAG, "emit " + i);
+                    emitter.onNext(i);
+                }
+            }
+        }, BackpressureStrategy.ERROR).subscribeOn(Schedulers.io())
+                .observeOn(AndroidSchedulers.mainThread()).subscribe(new Subscriber<Integer>() {
+            @Override
+            public void onSubscribe(Subscription s) {
+                Log.d(TAG, "onSubscribe");
+//                s.request(Long.MAX_VALUE);  //注意这句代码
+                //MissingBackpressureException 必须有
+                mSubscription = s;
+            }
+
+            @Override
+            public void onNext(Integer integer) {
+                Log.d(TAG, "onNext: " + integer);
+            }
+
+            @Override
+            public void onError(Throwable t) {
+                Log.w(TAG, "onError: ", t);
+            }
+
+            @Override
+            public void onComplete() {
+                Log.d(TAG, "onComplete");
+            }
+        });
+    }
+
+    public static void request(long n) {
+        mSubscription.request(n); //在外部调用request请求上游
+    }
+
 
 }
